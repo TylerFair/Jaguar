a2eb683fadd579665ff027da34a50adc55bc39fb
TylerFair <tyler.fairnington@gmail.com>
2025-05-05 16:28:06 +1000

    fixing more

---
+++ setup.py
@@ -0,0 +1,30 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="jaguar",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "jax[cuda12]<=0.4.31",
+        "jaxopt",
+        "jaxoplanet",
+        "numpyro",
+        "numpyro_ext",
+        "arviz",            
+        "exotic_ld",
+        "astropy",          
+        "corner",           
+        "jmespath",         
+        "exotedrf[stage4]",  
+        "celerite2",        
+        "matplotlib",       
+        "pandas",           
+        "pyyaml",           
+    ],
+    entry_points={
+        "console_scripts": [
+            "run-stage4 = jaguar.Stage4.runstage4:main",
+        ],
+    },
+)
