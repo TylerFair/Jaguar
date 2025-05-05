1fd2c76b7a780f397a383ab6905d6c157c837307
Tyler Fairnington <153495927+TylerFair@users.noreply.github.com>
2025-05-05 15:36:53 +1000

    Update __init__.py

--- Stage4/__init__.py
+++ Stage4/__init__.py
@@ -1,3 +1,3 @@
-from JaxWebb import runstage4 as runstage4
-from JaxWebb import unpack_trace as unpack_trace
-from JaxWebb import plotting as plotting
+from Jaguar import runstage4 as runstage4
+from Jaguar import unpack_trace as unpack_trace
+from Jaguar import plotting as plotting
